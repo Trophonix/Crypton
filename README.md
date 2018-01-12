c5df434f591634b852f85be22a690324fb8f1d03
Lucas Cosolo <502badgamer@gmail.com>
2018-01-11 23:34:12 -0600

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# Crypton
+
+Crypton is a [Discord](https://discordapp.com) bot for fetching and displaying prices of cryptocurrencies from exchanges through their public APIs and more crypto-related stuff.
+
+# How to Install
+
+Coming soon
+
+# How to Contribute
+
+Coming soon
+
+# Contact Me
+
+You can contact me, Trophonix in several ways:
+
+- Discord: `Lucas#5300`
+- Twitter: [@Trophonix](https://twitter.com/trophonix)
+- E-mail: [502badgamer@gmail.com](mailto:502badgamer@gmail.com)
+
+# Donate
+
+If you'd like to thank me for this and/or any of my other public projects, I would very much appreciate it.
+
+PayPal: [trophonix.paypal.me](trophonix.paypal.me/5)
+
+Ko-Fi: [Trophonix](https://ko-fi.com/trophonix)
+
+DOGE: `DSjcXTzYQ8N1a1UdwcNaYoaQofJRF99JHU`
---
+++ index.js
@@ -0,0 +1,65 @@
+const config = require('config.json');
+
+const Discord = require('discord.js');
+const RichEmbed = Discord.RichEmbed;
+const bot = new Discord.Client();
+
+const BinanceAPI = require('node-binance-api');
+BinanceAPI.options(config.binance);
+
+// Turn my pretty keys into compatible ones
+config.token.APIKEY = config.token.api_key;
+config.token.APISECRET = config.token.api_secret;
+
+bot.on('ready', () => {
+    bot.user.setPresence({
+        status: 'online',
+        game: {
+            name: `${config.prefix} help`
+        }
+    })
+    console.log('Connected to discord.');
+});
+
+bot.on('message', event => {
+    let message = event.content.toLowerCase();
+    if (message.startsWith(config.prefix)) {
+        message = message.replace(config.prefix, '');
+        let args = message.split(' ');
+        let command = args[0];
+        args.splice(0, 1);
+        event.guild.fetchMember(event.author).then(member => {
+            switch (command) {
+                case 'price':
+                    if (args.length == 0) {
+                        event.channel.sendEmbed(new RichEmbed()
+                            .setColor(config.colors.error)
+                            .setDescription('Incorrect usage!')
+                            .addField('Syntax', `${config.prefix} price (currency) (base; default: btc)`, true)
+                            .addField('Example', `${config.prefix} price xrp eth`, true)
+                            .setAuthor('Requested by ' + member.nickname, event.author.avatarURL));
+                        return;
+                    }
+                    
+                    let currency = args[0].topUpperCase();
+                    let defaultBase = args[1] || config.default_base.toUpperCase() || "BTC";
+
+                    BinanceAPI.prices(ticker => {
+                        let price = ticker[currency + defaultBase.toUpperCase()];
+                        let embed = new RichEmbed()
+                            .setColor(config.colors.main)
+                            .addField(currency + '/' + defaultBase.toUpperCase(), price, true);
+                        (config.other_base_displays || []).forEach(base => {
+                            if (!base.equalsIgnoreCase(defaultBase)) {
+                                embed.addField(currency.toUpperCase() + '/' + base.toUpperCase(), ticker[currency + base.toUpperCase()], true);
+                            }
+                        });
+                        event.channel.sendEmbed()
+                    });
+                    break;
+            }
+        }).catch(console.error);
+    }
+});
+
+bot.login(config.token);
