fb63a1933f1db348737d55c8c59df89a7aaffc8b
Lucas Cosolo <502badgamer@gmail.com>
2018-01-12 01:12:42 -0600

    parseFloat

--- index.js
+++ index.js
@@ -29,8 +29,7 @@ function getPrice(currency, base, decimals) {
     } else {
         price = prices[currency + base];
     }
-    console.log(typeof price);
-    return price.toFixed(decimals);
+    return parseFloat(price).toFixed(decimals);
 }
 
 bot.on('ready', () => {
