77bfdbe094ad731143ce8256f2f0cc06bafb59c1
Lucas Cosolo <502badgamer@gmail.com>
2018-01-12 13:47:56 -0600

    redirect subdomain to /callback

--- index.js
+++ index.js
@@ -5,7 +5,13 @@ const site = Express();
 
 site.get('/callback', (req, res) => res.redirect('https://github.com/Trophonix/Crypton/blob/master/CALLBACK.md'));
 
-site.get('/', (req, res) => res.redirect('https://github.com/Trophonix/Crypton'));
+site.get('/', (req, res) => {
+    if (req.subdomains && req.subdomains.length > 0 && req.subdomains[0].toLowerCase() === 'callback') {
+        res.redirect('/callback');
+        return;
+    }
+    res.redirect('https://github.com/Trophonix/Crypton');
+});
 
 site.listen(config.port || 5000, () => console.log(`Listening on port ${config.port}`));
 
