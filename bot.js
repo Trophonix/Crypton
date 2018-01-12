87401916bb694554aa84be2aafc5b92038ec7a38
Lucas Cosolo <502badgamer@gmail.com>
2018-01-12 14:53:53 -0600

    Wrap in a function

--- bot.js
+++ bot.js
@@ -1,68 +1,72 @@
-const config = require('./config.json');
+function bot() {
+    const config = require('./config.json');
 
-const Files = require('fs');
+    const Files = require('fs');
 
-const Discord = require('discord.js');
-const bot = new Discord.Client();
+    const Discord = require('discord.js');
+    const bot = new Discord.Client();
 
-const BinanceAPI = require('node-binance-api');
+    const BinanceAPI = require('node-binance-api');
 
-// Turn my pretty keys into compatible ones
-config.binance.APIKEY = config.binance.api_key;
-config.binance.APISECRET = config.binance.api_secret;
-BinanceAPI.options(config.binance);
+    // Turn my pretty keys into compatible ones
+    config.binance.APIKEY = config.binance.api_key;
+    config.binance.APISECRET = config.binance.api_secret;
+    BinanceAPI.options(config.binance);
 
-BinanceAPI.cache = {};
-getPrices();
+    BinanceAPI.cache = {};
+    getPrices();
 
-function getPrices() {
-    BinanceAPI.prices(ticker => {
-        prices = ticker;
+    function getPrices() {
+        BinanceAPI.prices(ticker => {
+            prices = ticker;
+        });
+    }
+    setInterval(getPrices, 5000);
+
+    const commands = [];
+    Files.readdir('./commands/', (err, files) => {
+        if (err) {
+            console.error(err);
+            process.exit(1);
+            return;
+        }
+
+        files.forEach((file, index) => {
+            console.log(file);
+            if (file.endsWith('.command.js')) {
+                commands.push(require('./commands/' + file)(this, config));
+            }
+        });
     });
+
+    bot.on('ready', () => {
+        bot.user.setPresence({
+            status: 'online',
+            game: {
+                name: `${config.prefix}help`
+            }
+        })
+        console.log('Connected to discord.');
+    });
+
+    bot.on('message', event => {
+        let message = event.content.toLowerCase();
+        if (message.startsWith(config.prefix)) {
+            message = message.replace(config.prefix, '');
+            let args = message.split(' ');
+            let cmd = args[0];
+            args.splice(0, 1);
+            event.guild.fetchMember(event.author).then(member => {
+                commands.forEach(command => {
+                    if (command.aliases.indexOf(cmd.toLowerCase()) !== -1) {
+                        command.onCommand(event, member, event.channel, args);
+                    }
+                });
+            }).catch(console.error);
+        }
+    });
+
+    bot.login(config.token);
 }
-setInterval(getPrices, 5000);
 
-const commands = [];
-Files.readdir('./commands/', (err, files) => {
-    if (err) {
-        console.error(err);
-        process.exit(1);
-        return;
-    }
-
-    files.forEach((file, index) => {
-        console.log(file);
-        if (file.endsWith('.command.js')) {
-            commands.push(require('./commands/' + file)(this, config));
-        }
-    });
-});
-
-bot.on('ready', () => {
-    bot.user.setPresence({
-        status: 'online',
-        game: {
-            name: `${config.prefix}help`
-        }
-    })
-    console.log('Connected to discord.');
-});
-
-bot.on('message', event => {
-    let message = event.content.toLowerCase();
-    if (message.startsWith(config.prefix)) {
-        message = message.replace(config.prefix, '');
-        let args = message.split(' ');
-        let cmd = args[0];
-        args.splice(0, 1);
-        event.guild.fetchMember(event.author).then(member => {
-            commands.forEach(command => {
-                if (command.aliases.indexOf(cmd.toLowerCase()) !== -1) {
-                    command.onCommand(event, member, event.channel, args);
-                }
-            });
-        }).catch(console.error);
-    }
-});
-
-bot.login(config.token);
+bot();
