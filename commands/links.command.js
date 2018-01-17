590bbd9e318dc4241c5f9269e03e4b7e3beda3af
Lucas Cosolo <502badgamer@gmail.com>
2018-01-16 22:50:03 -0600

    Add kucoin

--- commands/links.command.js
+++ commands/links.command.js
@@ -10,8 +10,9 @@ module.exports = (bot, config) => {
                 .setColor(config.colors.main)
                 .setDescription('Here are some crypto-related sites! Using these links supports the developer of this bot, and some of them give you a reward as well!')
                 .addField('Buy crypto with a credit card', 'https://www.buycryp.to | This site lets you buy the top coins with a credit card almost instantly!')
-                .addField('Trade crypto', 'https://www.binance.com/?ref=16393003 | Trade large and small coins with nice and response interfaces and helpful support.')
-                .addField('Trade smaller coins', 'https://cryptopia.site | Another exchange - this one hosts much smaller coins and icos, but be careful which you invest in.')
+                .addField('Binance (Exchange)', 'https://www.binance.com/?ref=16393003 | Trade large and small coins with nice and response interfaces and helpful support.')
+                .addField('KuCoin (Exchange)', 'https:`//kucoin.rocks | Awesome exchange which supports smaller coins and has a cool proof of stake system built-in with their own cryptocurrency.')
+                .addField('Cryptopia (Exchange)', 'https://cryptopia.site | Another exchange - this one hosts much smaller coins and icos, but be careful which you invest in.')
                 .addField('Store your coins more securely', 'https://ledgernano.club | The Ledger Nano S is a highly-rated hardware wallet for storing cryptocurrency safely.')
                 .setAuthor("Requested by " + member.displayName, member.user.avatarUrl)
                 .setTimestamp();
