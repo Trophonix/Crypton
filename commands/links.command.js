332886e3627c175b578c2e304bfbc12242706a8c
Lucas Cosolo <502badgamer@gmail.com>
2018-01-17 17:27:32 -0600

    Fix typo

--- commands/links.command.js
+++ commands/links.command.js
@@ -10,7 +10,7 @@ module.exports = (bot, config) => {
                 .setColor(config.colors.main)
                 .setDescription('Here are some crypto-related sites! Using these links supports the developer of this bot, and some of them give you a reward as well!')
                 .addField('Buy crypto with a credit card', 'https://www.buycryp.to | This site lets you buy the top coins with a credit card almost instantly!')
-                .addField('Binance (Exchange)', 'https://www.binance.com/?ref=16393003 | Trade large and small coins with nice and response interfaces and helpful support.')
+                .addField('Binance (Exchange)', 'https://www.binance.com/?ref=16393003 | Trade large and small coins with responsive interfaces and helpful support.')
                 .addField('KuCoin (Exchange)', 'http://kucoin.rocks | Awesome exchange which supports smaller coins and has a cool proof of stake system built-in with their own cryptocurrency.')
                 .addField('Cryptopia (Exchange)', 'http://cryptopia.site | Another exchange - this one hosts much smaller coins and icos, but be careful which you invest in.')
                 .addField('Store your coins more securely', 'http://ledgernano.club | The Ledger Nano S is a highly-rated hardware wallet for storing cryptocurrency safely.')
