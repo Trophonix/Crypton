2d0a7b337a4eba7a562663ecb28fc183ab48fa39
Lucas Cosolo <502badgamer@gmail.com>
2018-01-16 22:26:31 -0600

    Add ledger nano to $links

--- commands/links.command.js
+++ commands/links.command.js
@@ -12,6 +12,7 @@ module.exports = (bot, config) => {
                 .addField('Buy crypto with a credit card', 'https://www.buycryp.to | This site lets you buy the top coins with a credit card almost instantly!')
                 .addField('Trade crypto', 'https://www.binance.com/?ref=16393003 | Trade large and small coins with nice and response interfaces and helpful support.')
                 .addField('Trade smaller coins', 'https://cryptopia.site | Another exchange - this one hosts much smaller coins and icos, but be careful which you invest in.')
+                .addField('Store your coins more securely', 'https://ledgernano.club | The Ledger Nano S is a highly-rated hardware wallet for storing cryptocurrency safely.')
                 .setAuthor("Requested by " + member.displayName, member.user.avatarUrl)
                 .setTimestamp();
             channel.send(embed);
