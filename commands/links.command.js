ce930e69801171fc7711d1d902dae2f560c2bf30
Lucas Cosolo <502badgamer@gmail.com>
2018-01-16 22:24:03 -0600

    Add $links

---
+++ commands/links.command.js
@@ -0,0 +1,20 @@
+const RichEmbed = require('discord.js').RichEmbed;
+
+module.exports = (bot, config) => {
+    return {
+        aliases: ['links'],
+        usage: 'links',
+        description: 'Get links to sites for buying and trading crypto!',
+        onCommand: (event, member, channel, args) => {
+            let embed = new RichEmbed()
+                .setColor(config.colors.main)
+                .setDescription('Here are some crypto-related sites! Using these links supports the developer of this bot, and some of them give you a reward as well!')
+                .addField('Buy crypto with a credit card', 'https://www.buycryp.to | This site lets you buy the top coins with a credit card almost instantly!')
+                .addField('Trade crypto', 'https://www.binance.com/?ref=16393003 | Trade large and small coins with nice and response interfaces and helpful support.')
+                .addField('Trade smaller coins', 'https://cryptopia.site | Another exchange - this one hosts much smaller coins and icos, but be careful which you invest in.')
+                .setAuthor("Requested by " + member.displayName, member.user.avatarUrl)
+                .setTimestamp();
+            channel.send(embed);
+        }
+    }
+}
