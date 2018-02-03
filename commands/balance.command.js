e8f38f54283458afe4359e0da966e4e256b68474
Lucas Cosolo <502badgamer@gmail.com>
2018-02-02 23:41:28 -0600

    wait lets try this

--- commands/balance.command.js
+++ commands/balance.command.js
@@ -2,13 +2,13 @@ const RichEmbed = require('discord.js').RichEmbed;
 const cache = require('../cache');
 
 module.exports = (bot, config) => {
+  const block_io = require('block_io');
+  const BlockIO = new block_io(config.block_io.API_KEY, config.block_io.SECRET, 2);
   return {
     aliases: ['balance', 'bal'],
     usage: 'balance',
     description: 'View your balance',
     onCommand: (event, member, channel, args) => {
-      var block_io = require('block_io');
-      var BlockIO = block_io(config.block_io.API_KEY, config.block_io.SECRET, 2);
       BlockIO.get_address_balance({'label': member.id}, res => {
         if (res.status === 'success' && res.data) {
           let embed = new RichEmbed()
