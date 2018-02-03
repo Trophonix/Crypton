64432a22108911ef16fe3b70b66ef83fecff4a4b
Lucas Cosolo <502badgamer@gmail.com>
2018-02-02 23:32:04 -0600

    oh

--- commands/balance.command.js
+++ commands/balance.command.js
@@ -1,19 +1,17 @@
 const RichEmbed = require('discord.js').RichEmbed;
 const cache = require('../cache');
 
-const BlockIO = require('block_io')(config.block_io.API_KEY, config.block_io.SECRET, 2);
-
 module.exports = (bot, config) => {
+  const BlockIO = require('block_io')(config.block_io.API_KEY, config.block_io.SECRET, 2);
   return {
     aliases: ['balance', 'bal'],
     usage: 'balance',
     description: 'View your balance',
     onCommand: (event, member, channel, args) => {
-      let color = config.colors.main;
       BlockIO.get_address_balance({'label': member.id}, res => {
         if (res.status === 'success' && res.data) {
           let embed = new RichEmbed()
-            .setColor(color)
+            .setColor(config.colors.main)
             .setTitle('Your balance')
             .addField('Balance', res.data.available_balance);
           let pending = res.data.pending_received_balance;
