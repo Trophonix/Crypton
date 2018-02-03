caf6781842f77bad2fd9e0e371aa706dc51c6227
Lucas Cosolo <502badgamer@gmail.com>
2018-02-03 01:49:17 -0600

    endpoints

--- block_io.js
+++ block_io.js
@@ -2,7 +2,7 @@ const config = require('./config.json');
 const request = require('request');
 
 var API_KEY = config.block_io.API_KEY;
-var url = 'https://block.io/api/v2?api_key=' + API_KEY;
+var url = 'https://block.io/api/v2';
 
 var methods = {};
 
@@ -12,18 +12,18 @@ var appendVariable = (reqUrl, name, value) => {
 }
 
 methods.createWallet = (label, callback) => {
-  let reqUrl = url;
+  let reqUrl = url + '/get_new_address?api_key=' + API_KEY;
   reqUrl = appendVariable(reqUrl, 'label', label);
   request.post(reqUrl, (err, res) => {
-    callback(res);
+    callback(res.body);
   });
 }
 
 methods.getWalletBalance = (label, callback) => {
-  let reqUrl = url;
+  let reqUrl = url + '/get_address_balance?api_key=' + API_KEY;
   reqUrl = appendVariable(reqUrl, 'labels', label);
   request.post(reqUrl, (err, res) => {
-    callback(res);
+    callback(res.body);
   });
 }
 
