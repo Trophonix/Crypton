cdb269a586101019eae21d66bbbfe27c131e7f89
Lucas Cosolo <502badgamer@gmail.com>
2018-02-03 01:39:55 -0600

    require config

--- block_io.js
+++ block_io.js
@@ -1,29 +1,26 @@
+const config = require('./config.json');
 const request = require('request');
 
-module.exports = (config) => {
-  var API_KEY = config.block_io.API_KEY;
-  var url = 'https://block.io/api/v2?api_key=' + API_KEY;
+var API_KEY = config.block_io.API_KEY;
+var url = 'https://block.io/api/v2?api_key=' + API_KEY;
 
-  var appendVariable = (reqUrl, name, value) => {
-    reqUrl += '&' + name + '=' + value;
-    return reqUrl;
-  }
+var appendVariable = (reqUrl, name, value) => {
+  reqUrl += '&' + name + '=' + value;
+  return reqUrl;
+}
 
-  var createWallet = (label, callback) => {
-    let reqUrl = url;
-    reqUrl = appendVariable(reqUrl, 'label', label);
-    request.post(reqUrl, (err, res) => {
-      callback(res);
-    });
-  }
+var createWallet = (label, callback) => {
+  let reqUrl = url;
+  reqUrl = appendVariable(reqUrl, 'label', label);
+  request.post(reqUrl, (err, res) => {
+    callback(res);
+  });
+}
 
-  var getWalletBalance = (label, callback) => {
-    let reqUrl = url;
-    reqUrl = appendVariable(reqUrl, 'labels', label);
-    request.post(reqUrl, (err, res) => {
-      callback(res);
-    });
-  }
-
-  return this;
+var getWalletBalance = (label, callback) => {
+  let reqUrl = url;
+  reqUrl = appendVariable(reqUrl, 'labels', label);
+  request.post(reqUrl, (err, res) => {
+    callback(res);
+  });
 }
